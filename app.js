f4ff811aedc567013d2663702e240f3ad38d4e99
Andy Pecover <andypec@tiscali.co.uk>
2020-11-10 13:12:24 +0000

    Update to files

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-const geocode = require('./utils/geocode');
+const geocode = require('./utils/geocode'); 
 const forecast = require('./utils/forecast');
 
 const app = express();
 const port = process.env.PORT || 3000; 
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '/templates/views');
